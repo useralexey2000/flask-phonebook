316b9addd03866c9c68d75f39a8bf674da8e5516
useralexey2000 <useralexey2000@gmail.com>
2020-10-11 20:01:14 +0300

    Adding mongo to app

--- flphone/__init__.py
+++ flphone/__init__.py
@@ -1,22 +1,26 @@
 import os
 from flask import Flask
+import sys
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY = 'dev',
     )
 
+    if test_config is None:
+        app.config.from_pyfile('settings.py', silent=False)
+    else:
+        app.config.from_mapping(test_config)
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-
-    @app.route('/hello')
-    def say_hello():
-        return 'Hello, World!'
+    from . import db
+    db.mongo.init_app(app)
+    db.init_db(app)
     
     from . import pbook
     app.register_blueprint(pbook.bp)

c7cb4af77e0db32132a715724db03867aa9ed358
useralexey2000 <useralexey2000@gmail.com>
2020-10-11 19:58:04 +0300

    Changing db operations in  views to mongo methods

--- flphone/pbook.py
+++ flphone/pbook.py
@@ -4,55 +4,14 @@ from flask import (
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, FieldList, FormField
 from wtforms.validators import DataRequired, NumberRange, Length
-import uuid
+from werkzeug.exceptions import abort
+
+from .db import mongo
+from bson.objectid import ObjectId
 
 
 bp = Blueprint('pbook', __name__)
 
-pbook_list = [
-    {   
-        "id": "03363e5a9d9c4f1ca7a8dc465116c370",
-        "uname": "user1",
-        "fname": "fuser1",
-        "lname": "luser1",
-        "dep": "dep1",
-        "pos": "pos1",
-        "phones": [
-            {"phone": 1111},
-            {"phone": 2222},
-            {"phone": 3333},
-        ],
-    },
-    {
-        "id": "37ab474c0d3c4ed0ba2b08ac99a738b5",
-        "uname": "user2",
-        "fname": "fuser2",
-        "lname": "luser2",
-        "dep": "dep1",
-        "pos": "pos2",
-        "phones": [
-            {"phone": 4444},
-        ]
-    },
-    # {
-    #     "id": "92c962e9587e45218707c38f1cc143e3",
-    #     "uname": "user3",
-    #     "fname": "fuser3",
-    #     "lname": "luser3",
-    #     "dep": "dep1",
-    #     "pos": "pos3",
-    #     "phones": [555, 666]
-    # },
-    # {
-    #     "id": "f34ebced615e41e9bca5ca09039f5254",
-    #     "uname": "user4",
-    #     "fname": "fuser4",
-    #     "lname": "luser4",
-    #     "dep": "dep1",
-    #     "pos": "pos4",
-    #     "phones": [777]
-    # },
-]
 
 class PhoneForm(FlaskForm):
     phone = IntegerField('phone:', [DataRequired()])
@@ -66,70 +25,65 @@ class ContactForm(FlaskForm):
     dep = StringField('department:', [DataRequired(), Length(min=4, max=20)])
     pos = StringField('position:', [DataRequired(), Length(min=4, max=30)])
     phones = FieldList(FormField(PhoneForm), min_entries=1, max_entries=5)
-    removep = SubmitField('remove')
-    addp = SubmitField('add')
     submit = SubmitField('save')
 
 
 @bp.route('/')
 def index():
-    return render_template('pbook/index.html', pbook_list=pbook_list)
+    col = mongo.db.fphone
+    res = list(col.find())
+    return render_template('pbook/index.html', pbook_list=res)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
 def create():
-    form  = ContactForm(request.form)
-
+    form  = ContactForm(request.form)   
     if request.method == 'POST':
-        
-        # if form.addp.data:
-        #     form.phones.append_entry()
-        #     form.addp.data = False
-        #     return render_template('pbook/create-edit.html', form=form)
-        # if form.removep.data:
-        #     form.phones.pop_entry()
-        #     form.removep.data = False
-        #     return render_template('pbook/create-edit.html', form=form)
         if form.validate_on_submit():
+            col = mongo.db.fphone
             entry = {}
-            entry['id'] = uuid.uuid4().hex
             entry['uname'] = form.uname.data
             entry['fname'] = form.fname.data
             entry['lname'] = form.lname.data
             entry['dep'] = form.dep.data
             entry['pos'] = form.pos.data
-            entry['phones'] = [form.phones.data]
-            pbook_list.append(entry)
+            entry['phones'] = form.phones.data
+            col.save(entry)
             return redirect('/')
     return render_template('pbook/create-edit.html', form=form, title='phone-create')
 
 
 @bp.route('/<id>/edit', methods=('GET', 'POST'))
 def edit(id):
-    res = [i for i in pbook_list if i['id'] == id]
-    if res:
-        if request.method == 'POST':
-            pass
-            # form = ContactForm(request.form)
-            # if form.addp.data:
-            #     form.phones.append_entry()
-            #     return render_template('pbook/edit.html', form=form)
-            # if form.removep.data:
-            #     form.phones.pop_entry()
-            #     return render_template('pbook/edit.html', form=form)
-            # if form.validate_on_submit():
-            #     return redirect('/')
-        form = ContactForm(data=res[0])
-        return render_template('pbook/create-edit.html', form=form, title='phone-edit')
+    col = mongo.db.fphone
+    res = col.find_one({'_id': ObjectId(id)})
+    if not res:
+        abort(404, 'the page not found.')
+    if request.method == 'POST':
+        form = ContactForm(request.form)
+        if form.validate_on_submit():
+            entry = {}
+            entry['uname'] = form.uname.data
+            entry['fname'] = form.fname.data
+            entry['lname'] = form.lname.data
+            entry['dep'] = form.dep.data
+            entry['pos'] = form.pos.data
+            entry['phones'] = form.phones.data
+            col.update_one({'_id': ObjectId(id)}, {'$set': entry })
+            return redirect('/')
+    form = ContactForm(data=res)
+    return render_template('pbook/create-edit.html', form=form, title='phone-edit')
 
 
 @bp.route('/<id>/delete', methods=('POST',))
 def delete(id):
-    res = [i for i in pbook_list if i['id'] == id]
-    if res:
-        pbook_list.remove(res[0])
+    col = mongo.db.fphone
+    res = col.find_one({'_id': ObjectId(id)})
+    if not res:
+        abort(404, 'the page not found.')
+    col.delete_one({'_id': ObjectId(id)})
     return redirect('/')
-
     
+
 
 

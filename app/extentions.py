6fdf5caf0a12f669099cfc4796baab350ef4b62f
useralexey2000 <useralexey2000@gmail.com>
2020-10-24 19:49:02 +0300

    deleting app/extentions.py

--- app/extentions.py
+++
@@ -1,9 +0,0 @@
-# from flask_pymongo import PyMongo
-from flask_mongoengine import MongoEngine
-from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
-
-
-db = MongoEngine()
-login_manager = LoginManager()
-bc = Bcrypt()

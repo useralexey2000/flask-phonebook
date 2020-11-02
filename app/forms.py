5a82e8274966483b0d73c64ab4a158cdce95f723
useralexey2000 <useralexey2000@gmail.com>
2020-11-02 16:54:44 +0200

    Adding register form to app/forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, FieldList, FormField, PasswordField
-from wtforms.validators import DataRequired, NumberRange, Length, Email
+from wtforms import StringField, IntegerField, SubmitField, FieldList, FormField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, NumberRange, Length, Email, EqualTo
 from wtforms.fields.html5 import EmailField
+from app.models import User, ACCESS
 
 
 class PhoneForm(FlaskForm):
@@ -19,6 +20,19 @@ class ContactForm(FlaskForm):
     submit = SubmitField('save')
 
 class LoginForm(FlaskForm):
-    email = EmailField('email', [DataRequired(), Email()])
+    email = EmailField('email:', [DataRequired(), Email()])
     password = PasswordField('password:', [DataRequired(), Length(max=20)])
+    remember_me = BooleanField('keep me logged in')
     submit = SubmitField('login')
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('email:', [DataRequired(), Email(), Length(max=100)])
+    password = PasswordField('password:', [DataRequired(), EqualTo('password2') , Length(max=20)])
+    password2 = PasswordField('confirm password:', [DataRequired(), Length(max=20)])
+    submit = SubmitField('register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email = field.data).first():
+            raise ValidationError('email already registered')
+

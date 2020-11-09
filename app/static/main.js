9238b72714bfa1b25255c810d5db36987c760218
useralexey2000 <useralexey2000@gmail.com>
2020-11-09 15:55:38 +0200

    Modyfying add/remove btns script because inserting font awsome tags in btns main.js

--- app/static/main.js
+++ app/static/main.js
@@ -5,12 +5,16 @@ document.addEventListener("DOMContentLoaded", function(){
     if(entryForm) {
         entryForm.addEventListener("click", function (e) {
         // remove phone elem
-            if (e.target.classList.contains("rem-phone")){
-                let rem = e.target.parentNode;
+            // if (e.target.classList.contains("rem-phone")){
+            if (e.target.closest(".rem-phone")){
+
+                let rem = e.target.closest(".rem-phone").parentNode;
                 this.removeChild(rem);
+                return;
     
         // add phone elem            
-            } else if (e.target.id == "add-phone") {
+            // } else if (e.target.id == "add-phone") {
+            } else if (e.target.closest('#add-phone')) {
                     
                 let items = this.getElementsByClassName("phone-item");
                 let n = items.length;
@@ -26,10 +30,13 @@ document.addEventListener("DOMContentLoaded", function(){
                 div.setAttribute("class", "phone-item");
                 
                 div.innerHTML = `<label for=`+ name +`>phone:</label>
-                <input class="phone-num" id=` + name + ` name=` + name + ` type="text" value="" required>
-                <a class="rem-phone" href="#">remove</a>`;
+                <a class="rem-phone" href="#">
+                    <i class="far fa-trash-alt"></i>
+                </a>
+                <input class="phone-num" id=` + name + ` name=` + name + ` type="text" value="" required>`;
     
                 this.insertBefore(div, this.lastElementChild.previousSibling);
+                return;
             }
         });
     }

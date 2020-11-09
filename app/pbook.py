5294b815c1afdfe459a9f58cd08443a3e0f1362d
useralexey2000 <useralexey2000@gmail.com>
2020-11-09 12:51:25 +0200

    Changing descendent order to ascendend in displaying contacts app/pbook.py

--- app/pbook.py
+++ app/pbook.py
@@ -12,7 +12,7 @@ bp = Blueprint('pbook', __name__)
 @bp.route('/')
 @bp.route('/<int:page>')
 def index(page=1):
-    contacts = Contact.query.order_by(Contact.id.desc()).paginate(page, per_page=30)
+    contacts = Contact.query.order_by(Contact.lname.asc()).paginate(page, per_page=30)
     return render_template('pbook/index.html', contacts=contacts)
 
 

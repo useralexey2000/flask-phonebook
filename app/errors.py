a3a504828ddfadd1a6ea8421703385f2b9d4bef5
useralexey2000 <useralexey2000@gmail.com>
2020-11-11 12:53:02 +0200

    Adding custom error pages and error blueprint

---
+++ app/errors.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+from werkzeug.exceptions import HTTPException
+
+bp = Blueprint('error', __name__)
+
+@bp.app_errorhandler(HTTPException)
+def handle_http_exception(e):
+    return render_template('http_error.html', e=e)
+
+@bp.app_errorhandler(Exception)
+def handle_generic_exception(e):
+    return render_template('generic_error.html', e=e), 500

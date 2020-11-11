8dc53cd88d4e43382d2580348140753058860b5c
useralexey2000 <useralexey2000@gmail.com>
2020-11-11 11:22:29 +0200

    Adding error messages to forms app/forms.py

--- app/forms.py
+++ app/forms.py
@@ -6,30 +6,30 @@ from app.models import User, ACCESS
 
 
 class PhoneForm(FlaskForm):
-    num = IntegerField('phone:', [DataRequired()])
+    num = IntegerField('phone:', [DataRequired(message='Field cant be blank')])
     class Meta:
         csrf = False
 
 class ContactForm(FlaskForm):
-    uname = StringField('first name:', [DataRequired(), Length(min=4, max=30)])
-    fname = StringField('father\'s name:', [DataRequired(), Length(min=4, max=30)])
-    lname = StringField('last name:', [DataRequired(), Length(min=4, max=30)])
-    dep = StringField('department:', [DataRequired(), Length(min=4, max=30)])
-    pos = StringField('position:', [DataRequired(), Length(min=4, max=30)])
+    uname = StringField('first name:', [DataRequired(message='Field cant be blank'), Length(min=4, max=30, message='Field lenght: min=4, max=30'),])
+    fname = StringField('father\'s name:', [DataRequired(message='Field cant be blank'), Length(min=4, max=30, message='Field lenght: min=4, max=30')])
+    lname = StringField('last name:', [DataRequired(message='Field cant be blank'), Length(min=4, max=30, message='Field lenght: min=4, max=30')])
+    dep = StringField('department:', [DataRequired(message='Field cant be blank'), Length(min=4, max=30, message='Field lenght: min=4, max=30')])
+    pos = StringField('position:', [DataRequired(message='Field cant be blank'), Length(min=4, max=30, message='Field lenght: min=4, max=30')])
     phones = FieldList(FormField(PhoneForm), min_entries=1, max_entries=5)
     submit = SubmitField('save')
 
 class LoginForm(FlaskForm):
-    email = EmailField('email:', [DataRequired(), Email()])
-    password = PasswordField('password:', [DataRequired(), Length(max=20)])
+    email = EmailField('email:', [DataRequired(message='Field cant be blank'), Email(message='Not correct email'), Length(max=100, message='Field lenght: max=100')])
+    password = PasswordField('password:', [DataRequired(message='Field cant be blank'), Length(max=20, message='Field lenght: max=20')])
     remember_me = BooleanField('keep me logged in:')
     submit = SubmitField('login')
 
 
 class RegisterForm(FlaskForm):
-    email = EmailField('email:', [DataRequired(), Email(), Length(max=100)])
-    password = PasswordField('password:', [DataRequired(), EqualTo('password2') , Length(max=20)])
-    password2 = PasswordField('confirm password:', [DataRequired(), Length(max=20)])
+    email = EmailField('email:', [DataRequired(message='Field cant be blank'), Email(message='Not correct email'), Length(max=100, message='Field lenght: max=100')])
+    password = PasswordField('password:', [DataRequired(message='Field cant be blank'), EqualTo('password2', message='Passwords don\'t match') , Length(max=20, message='Field lenght: max=20')])
+    password2 = PasswordField('confirm password:', [DataRequired(message='Field cant be blank'), Length(max=20, message='Field lenght: max=20')])
     submit = SubmitField('register')
 
     def validate_email(self, field):
@@ -38,8 +38,9 @@ class RegisterForm(FlaskForm):
 
 
 class UserForm(FlaskForm):
-    email = EmailField('email:', [DataRequired(), Email(), Length(max=100)])
-    password = PasswordField('password:', [DataRequired(), Length(max=20)])
+    email = EmailField('email:', [DataRequired(message='Field cant be blank'), Email(message='Not correct email'), Length(max=100, message='Field lenght: max=100')])
+    reset_password = BooleanField('change password:')
+    password = PasswordField('password:', [Length(max=20, message='Field lenght: max=20')])
     role = SelectField('role:', choices=[i for i in ACCESS.keys()])
     submit = SubmitField('save')
 
